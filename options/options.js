b2d35a5f4cc5dfbd907514270753f64401fa3ca0
Crowdbotics <team@crowdbotics.com>
2023-06-15 20:08:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "krysclick-generator-41517",
+  url: "https://krysclick-generator-41517.botics.co",
+  api: "https://krysclick-generator-41517.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
